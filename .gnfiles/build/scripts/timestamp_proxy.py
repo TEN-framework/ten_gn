cf1060add0869b8ec3aa986c551104b6f91c6d1a
Hu Yueh-Wei <wei.hu.tw@gmail.com>
2025-01-12 19:37:21 +0800

    feat: refine C++ extension standalone test building (#22)

--- .gnfiles/build/scripts/timestamp_proxy.py
+++ .gnfiles/build/scripts/timestamp_proxy.py
@@ -8,6 +8,8 @@ import os
 from typing import Optional
 from build.scripts import touch
 
+# A unique file extension is used to significantly reduce the likelihood of name
+# conflicts between timestamp files and non-timestamp files.
 TG_TIMESTAMP_PROXY_EXTENSION = ".tg_timestamp_proxy"
 
 
@@ -19,6 +21,15 @@ def _gen_timestamp_proxy_path(path: Optional[str]) -> Optional[str]:
 
 
 def touch_timestamp_proxy_file(path: Optional[str]) -> None:
+    """
+    A timestamp file is typically one of a target's outputs, and the act of
+    creating or updating the timestamp file ensures that GN, during the next
+    build, recognizes that the timestamp file is up-to-date and therefore
+    determines that the target does not need to be re-executed.
+
+    Args:
+        path (Optional[str]): The timestamp file path.
+    """
     path = _gen_timestamp_proxy_path(path)
 
     if path:
@@ -29,7 +40,18 @@ def touch_timestamp_proxy_file(path: Optional[str]) -> None:
 
 
 def remove_timestamp_proxy_file(path: Optional[str]) -> None:
+    """
+    A timestamp file is typically one of a target's outputs, and deleting the
+    timestamp file effectively causes GN to determine during the next build that
+    the target associated with that timestamp file needs to be executed again.
+
+    Args:
+        path (Optional[str]): The timestamp file path.
+    """
     path = _gen_timestamp_proxy_path(path)
 
     if path and os.path.exists(path):
-        os.remove(path)
+        try:
+            os.remove(path)
+        except Exception as e:
+            raise Exception(f"Failed to remove timestamp proxy file: {str(e)}")
